2d22f684179fdfe277c667d846ed788b71ffe2d8
SwiftySwizzy <mihailernandes@gmail.com>
2018-01-04 13:16:05 +0200

    Samples + help menu add

--- src/com/gmail/mihailernandes/main/Main.java
+++ src/com/gmail/mihailernandes/main/Main.java
@@ -1,6 +1,9 @@
 package com.gmail.mihailernandes.main;
 
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+
 
 import com.gmail.mihailernandes.abstr.AbstractFilter;
 import com.gmail.mihailernandes.impl.Sepia;
@@ -11,21 +14,26 @@ import com.gmail.mihailernandes.impl.Negative;
 public class Main {
 
 	public static void main(String[] args) {
-		String path = args[0];
-		String filterName = args[1];
-		
+
 		Hashtable<String, AbstractFilter> filterNames = new Hashtable<String, AbstractFilter>();
-		
+
 		filterNames.put("grayscale", new Grayscale(0.299, 0.587, 0.114));
 		filterNames.put("sepia", new Sepia());
 		filterNames.put("negative", new Negative());
 		filterNames.put("vintage", new Vintage());
-		
-		if (filterNames.containsKey(filterName)) {
-			AbstractFilter filter = filterNames.get(filterName);
-			filter.filter(path);
-		}
-		
-	}
 
+		for (String input : args) {
+			if (input.equals("help")) {
+				Iterator<Map.Entry<String, AbstractFilter>> it = filterNames.entrySet().iterator();	
+				while(it.hasNext()) {
+					Map.Entry<String, AbstractFilter> entry = it.next();
+					System.err.println(entry.getKey());
+				}
+			}
+			if (filterNames.containsKey(input)) {
+				AbstractFilter filter = filterNames.get(input);
+				filter.filter(args[0]);
+			}
+		}
+	}
 }
